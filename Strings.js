592ba1c42ba1e7852681fc20a1e047ce70f4aa0f
Stivliz <stivliz0909@gmail.com>
2022-08-11 12:55:25 -0500

    Se añadio un nuevo archivo

---
+++ Strings.js
@@ -0,0 +1,21 @@
+/* STRINGS:
+Los STRINGS ocadena de texto representan un texto que se puede asignar a una variable, cosas como el nombre de un cliente, 
+el nombre de un producto o la categoria. Todo eso es un texto y se representa por medio de un string, existen dos formas de crerar lo que se 
+conocen como una cadena de texto primitiva y existe otra que es poco menos comun.
+*/
+
+// 1. FORMA:
+
+const producto = 'hola';
+
+// 2. FORMA:
+
+const producto2 = string('hola')
+
+//3 FORMA:
+
+const producto3 = newString('hola')
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
